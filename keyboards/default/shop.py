9023c389c4c1e71cd1caa1ab1cf57031362d4d9b
Utkirbek10 <olimovotkirbek49@gmail.com>
2022-07-19 15:12:06 +0500

    initial commit

--- keyboards/default/shop.py
+++ keyboards/default/shop.py
@@ -3,7 +3,6 @@ from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
 menu = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text='💻 Kompyuter'),KeyboardButton(text='📱 Telefon')],
-        [KeyboardButton(text='📺 Televizor'),KeyboardButton(text='🚖 Mashina')]
     ],
     resize_keyboard= True
 )
