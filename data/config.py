02d12108292d6faa027f5d52e7de8b07c7c32e17
Utkirbek10 <olimovotkirbek49@gmail.com>
2022-07-24 14:58:45 +0500

    Deploy to heroku

--- data/config.py
+++ data/config.py
@@ -1,11 +1,10 @@
 from environs import Env
 
 # environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-PROVIDER_TOKEN=env.str("PROVIDER_TOKEN")
-PROVIDER_TOKEN_PAY_ME=env.str("PROVIDER_TOKEN_PAY_ME")
+BOT_TOKEN = "5131045834:AAE09QCxjp6MdjSWgfAgh2ZdrWMPHMD9G2U" # Bot toekn
+ADMINS = [5012333108,]  # adminlar ro'yxati
+PROVIDER_TOKEN="398062629:TEST:999999999_F91D8F69C042267444B74CC0B3C747757EB0E065"
+PROVIDER_TOKEN_PAY_ME="371317599:TEST:1646048231693"
